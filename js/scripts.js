d4386fde80ed4d69651a40927003420aee47aed7
Vitaliy Sulimenko <prevetal@gmail.com>
2022-08-05 16:31:03 +0300

    Initial commit

---
+++ js/scripts.js
@@ -0,0 +1,186 @@
+$(() => {
+	// Ширина окна для ресайза
+	WW = $(window).width()
+
+
+	// АРЕПЛИВИР И ЭСПЕРАВИР ВАШИМ ПАЦИЕНТАМ
+	if ($('.medicine .swiper').length) {
+		new Swiper('.medicine .swiper', {
+			loop: true,
+			speed: 500,
+			watchSlidesProgress: true,
+			slideActiveClass: 'active',
+			slideVisibleClass: 'visible',
+			spaceBetween: 24,
+			slidesPerView: 1,
+			navigation: {
+				nextEl: '.swiper-button-next',
+				prevEl: '.swiper-button-prev'
+			},
+			preloadImages: false,
+			lazy: {
+				enabled: true,
+				checkInView: true,
+				loadOnTransitionStart: true,
+				loadPrevNext: true
+			}
+		})
+	}
+
+
+	// Моб. меню
+	$('header .mob_menu_btn').click((e) => {
+		e.preventDefault()
+
+		$('header .mob_menu_btn').addClass('active')
+		$('body').addClass('menu_open')
+		$('header .mob_menu').addClass('show')
+		$('.overlay').fadeIn(300)
+	})
+
+	$('header .mob_close_btn, .overlay').click((e) => {
+		e.preventDefault()
+
+		$('header .mob_menu_btn').removeClass('active')
+		$('body').removeClass('menu_open')
+		$('header .mob_menu').removeClass('show')
+		$('.overlay').fadeOut(300)
+	})
+
+
+	// Fancybox
+	Fancybox.defaults.autoFocus = false
+	Fancybox.defaults.dragToClose = false
+	Fancybox.defaults.l10n = {
+		CLOSE: "Закрыть",
+		NEXT: "Следующий",
+		PREV: "Предыдущий",
+		MODAL: "Вы можете закрыть это модальное окно нажав клавишу ESC"
+	}
+
+	Fancybox.defaults.template = {
+		closeButton: '<svg><use xlink:href="images/sprite.svg#ic_close"></use></svg>',
+		spinner: '<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="25 25 50 50" tabindex="-1"><circle cx="50" cy="50" r="20"/></svg>',
+		main: null
+	}
+
+	// Всплывающие окна
+	$('body').on('click', '.modal_btn', function (e) {
+		e.preventDefault()
+
+		Fancybox.close()
+
+		Fancybox.show([{
+			src: $(this).data('modal'),
+			type: 'inline'
+		}])
+	})
+
+
+	// Календарь
+	const calendar = new AirDatepicker('#calendar', {
+		inline: true,
+		showOtherMonths: false,
+		selectOtherMonths: false,
+		multipleDates: true,
+		// startDate: '01.05.2022'
+	})
+
+
+	$('.calendar .months .btn').click(function (e) {
+		e.preventDefault()
+
+		let date = $(this).data('date')
+
+		$('.calendar .months .btn').removeClass('active')
+		$(this).addClass('active')
+
+		calendar.setViewDate(date)
+
+	})
+
+
+	// Маска ввода
+	$('input[type=tel]').inputmask('+7 (999) 999-99-99')
+
+	// Кастомный select
+	$('select').niceSelect()
+
+	// Фокус при клике на название поля
+	$('body').on('click', '.form .label', function () {
+		$(this).closest('.line').find('.input, textarea').focus()
+	})
+
+	// Выбор файла
+	$('body').on('change', '.form input[type=file]', function (e) {
+		$(this).closest('.file').find('label').text($(this).val())
+	})
+
+
+	if (is_touch_device()) {
+		// Подменю на тач скрине
+		$('header .menu_item > a.sub_link').addClass('touch_link')
+
+		$('header .menu_item > a.sub_link').click(function (e) {
+			const $dropdown = $(this).next()
+
+			if ($dropdown.css('visibility') === 'hidden') {
+				e.preventDefault()
+
+				$('header .menu .sub_menu').removeClass('show')
+				$dropdown.addClass('show')
+
+				$('body').css('cursor', 'pointer')
+			}
+		})
+
+		// Закрываем под. меню при клике за её пределами
+		$(document).click((e) => {
+			if ($(e.target).closest('.menu').length === 0) {
+				$('header .menu .sub_menu').removeClass('show')
+
+				$('body').css('cursor', 'default')
+			}
+		})
+
+
+		// Закрытие моб. меню свайпом справо на лево
+		let ts
+
+		$('body').on('touchstart', (e) => { ts = e.originalEvent.touches[0].clientX })
+
+		$('body').on('touchend', (e) => {
+			let te = e.originalEvent.changedTouches[0].clientX
+
+			if ($('body').hasClass('menu_open') && ts > te + 50) {
+				// Свайп справо на лево
+				$('header .mob_menu_btn').removeClass('active')
+				$('body').removeClass('menu_open')
+				$('header .mob_menu').removeClass('show')
+				$('.overlay').fadeOut(300)
+			} else if (ts < te - 50) {
+				// Свайп слева на право
+			}
+		})
+	}
+})
+
+
+
+$(window).on('resize', () => {
+	if (typeof WW !== 'undefined' && WW != $(window).width()) {
+		// Моб. версия
+		if (!firstResize) {
+			$('meta[name=viewport]').attr('content', 'width=device-width, initial-scale=1, maximum-scale=1')
+			if ($(window).width() < 480) $('meta[name=viewport]').attr('content', 'width=480, user-scalable=no')
+
+			firstResize = true
+		} else {
+			firstResize = false
+		}
+
+
+		// Перезапись ширины окна
+		WW = $(window).width()
+	}
+})
