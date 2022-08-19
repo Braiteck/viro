92926e5cc46eb41ad48d647b05ce79accfa94362
Vitaliy Sulimenko <prevetal@gmail.com>
2022-08-19 10:19:49 +0300

    Фикс. шапка

--- js/scripts.js
+++ js/scripts.js
@@ -249,6 +249,21 @@ $(() => {
 
 
 
+$(window).on('load', () => {
+	// Фикс. шапка
+	headerInit = true,
+		headerHeight = $('header').outerHeight()
+
+	$('header').wrap('<div class="header_wrap"></div>')
+	$('.header_wrap').height(headerHeight)
+
+	headerInit && $(window).scrollTop() > headerHeight
+		? $('header').addClass('fixed')
+		: $('header').removeClass('fixed')
+})
+
+
+
 $(window).on('resize', () => {
 	if (typeof WW !== 'undefined' && WW != $(window).width()) {
 		// Моб. версия
@@ -262,7 +277,31 @@ $(window).on('resize', () => {
 		}
 
 
+		// Фикс. шапка
+		headerInit = false
+		$('.header_wrap').height('auto')
+
+		setTimeout(() => {
+			headerInit = true
+			headerHeight = $('header').outerHeight()
+
+			$('.header_wrap').height(headerHeight)
+
+			headerInit && $(window).scrollTop() > headerHeight
+				? $('header').addClass('fixed')
+				: $('header').removeClass('fixed')
+		}, 100)
+
+
 		// Перезапись ширины окна
 		WW = $(window).width()
 	}
+})
+
+
+$(window).scroll(() => {
+	// Фикс. шапка
+	typeof headerInit !== 'undefined' && headerInit && $(window).scrollTop() > headerHeight
+		? $('header').addClass('fixed')
+		: $('header').removeClass('fixed')
 })
